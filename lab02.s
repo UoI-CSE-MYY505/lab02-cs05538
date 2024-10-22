fd19adb90ff466b300fd69fe1cbc5caf984555ac
cs05538 <cs05538@uoi.gr>
2024-10-22 21:04:36 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -10,8 +10,20 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+    li s0, 0
+    li s1, 7
+loop:
+    blt s1, zero, done
+    slli t0, s1, 2
+    add t0, t0, a0
+    lw t1, 0(t0)
+    bne t1, a2, count
+    add s0, s0 , t0
+    j done
+
+count:
+    addi s1, s1, -1
+    j loop
 #-----------------------------
 done:
     addi a7, zero, 10 
